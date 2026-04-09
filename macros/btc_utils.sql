4d8dc4713565da66a66fe04e085ad608012c4b87
Tanmay Pathak <tanmaypathak@Tanmays-MacBook-Pro.local>
2026-04-09 21:44:34 +0300

    whale alert produce

---
+++ macros/btc_utils.sql
@@ -0,0 +1,11 @@
+{% macro convert_to_usd(column_name)%}
+
+{{column_name}} * (
+
+    select
+    PRICE
+    from {{ref("btc_usd_max")}}
+    where to_date(replace(snapped_at,'UTC',''))= current_date()
+)
+
+{% endmacro %}
